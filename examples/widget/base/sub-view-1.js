bc1873fb9b3f0aba69a0df91f7ead3bd7b52a967
Benny <yilei.bao@outlook.com>
2017-11-01 01:11:45 +0800

    测试例子

--- examples/widget/base/sub-view-1.js
+++ examples/widget/base/sub-view-1.js
@@ -1,3 +1,14 @@
-cola(function (model) {
-
+cola(function (model, param) {
+	debugger;
+	model.dataType({
+		name: "TestDataType",
+		properties: {
+			id: {
+				caption: "ID"
+			},
+			name: {
+				caption: "Name"
+			}
+		}
+	});
 });
